a02964f8e6dd9ff81dba4a7721332734c1d8246d
brunamanuella2-cyber <brunamanuella2@gmail.com>
2026-06-10 11:29:20 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const frases = [
+  "I feel more confident every day.",
+  "I speak English more naturally now.",
+  "I learn new words very quickly.",
+  "Every day I understand English better.",
+  "I enjoy practicing English daily."
+];
+
+let indice = 0;
+
+function mostrarFrase() {
+  document.getElementById("frase").innerText = frases[indice];
+}
+
+function proximaFrase() {
+  indice++;
+
+  if (indice >= frases.length) {
+    indice = 0;
+  }
+
+  mostrarFrase();
+}
+
+window.onload = mostrarFrase;
